ad67781b2094159b6ba928d2b4cf2b50c2e91d37
Daniel Bruder <daniel.bruder@gmail.com>
2013-03-31 16:44:54 +0200

    own stuff

--- custom/dbruder.aliases.autossh.zsh
+++ custom/dbruder.aliases.autossh.zsh
@@ -17,9 +17,11 @@ typeset -A HOST_USER
 
 HOST_PORT=(
     butterpilz  22222
-    crux        52195
     diener      22223
-    calculus    22555
+    calculus    22224
+    uschebti    22225
+    raspr       22226
+    crux        52195
 )
 
 FULL_HOST=(
@@ -27,6 +29,8 @@ FULL_HOST=(
     crux        uberspace.de
     diener      cis.uni-muenchen.de
     calculus    cis.uni-muenchen.de
+    uschebti    cis.uni-muenchen.de
+    ra    cis.uni-muenchen.de
 )
 
 HOST_USER=(
@@ -54,6 +58,28 @@ assh () {
         # return
         # AUTOSSH_POLL=$AUTOSSH_POLLTIME autossh -M $PORT $HOST
     fi
+}
+
+assh2 () {
+    set -x
+    exec autossh -M 22222 `perl -e '$host=shift; $_=do{local(@ARGV, $/)="$ENV{HOME}/.ssh/config"; <>}; m<^Host\s+(?<Host>$host)\s*HostName\s+(?<HostName>.*$)\s*User\s+(?<User>.*$)\s*>mgx; print "$+{User}\@$+{HostName}"' $1`
+    # $HOST=$1; shift
+    # PORT=${HOST_PORT[$HOST]:=22222}
+    # autossh -M $PORT $HOST
+    # if [[ -z "$@" ]]; then  # If no arguments are given
+    #     echo 'Select your host:'
+    #     PS3='host? '
+    #     select HOST in ${(k)HOST_PORT}; do
+    #         AUTOSSH_POLL=$AUTOSSH_POLL autossh -M ${HOST_PORT[$HOST]} ${HOST_USER[$HOST]}@$HOST.${FULL_HOST[$HOST]}
+    #     done
+    # else
+    #     HOST=$1; shift
+    #     PORT=${HOST_PORT[$HOST]}
+    #     [[ -z $PORT ]] && echo "Port to use for host $HOST is not known. Aborting." && return
+    #     AUTOSSH_POLL=$AUTOSSH_POLL autossh -M ${HOST_PORT[$HOST]} ${HOST_USER[$HOST]}@$HOST.${FULL_HOST[$HOST]}
+    #     # return
+    #     # AUTOSSH_POLL=$AUTOSSH_POLLTIME autossh -M $PORT $HOST
+    # fi
 }
 # AUTOSSH_POLL=5 autossh -M 22222 bruder@calculus.cis.uni-muenchen.de
 # AUTOSSH_POLL=5 autossh -M 22222 bruder@eselsohr.cip.ifi.lmu.de
